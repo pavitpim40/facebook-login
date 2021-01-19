6c296fc3224b724f804f1ba4d1e2acc5c0f364e9
pavit <pavitpim@gmail.com>
2021-01-19 22:05:07 +0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  const handleUser = () => {
+    window.FB.api('/me', function (res) {
+      console.log("TOKEN =>>", window.FB.getAccessToken());
+      console.log('Good to see you, ' + res.name + '.');
+    });
+  }
+
+  const onClickFB = () => {
+    window.FB.getLoginStatus(function (response) {
+      console.log(response)
+      if (response.status === "connected") {
+        handleUser()
+      } else {
+        window.FB.login(function (response) {
+          handleUser()
+        },{scope: 'public_profile,email'});
+      }
+    });
+  }
+
+  return (
+    <div className="App">
+      <button onClick={onClickFB}>LOGIN</button>
+    </div>
+  );
+}
+
+export default App;
